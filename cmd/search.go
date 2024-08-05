fed2a34ac644634c9ce9898bc3ebdb013591ae48
willie <oncwnuO8nwe_CeYIXTeAWePo9Cd0@git.weixin.qq.com>
2024-08-05 22:29:18 +0800

    assoc/search 完成mind小工具帮助搞定忘词

--- cmd/search.go
+++ cmd/search.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/user"
 
@@ -22,8 +21,8 @@ var searchCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		//和assoc关联的功能
-		// 打开文件
-		// 获取当前用户的主目录路径
+		//打开文件
+		//获取当前用户的主目录路径
 		usr, err := user.Current()
 		if err != nil {
 			fmt.Println("获取用户信息失败：", err)
@@ -38,57 +37,65 @@ var searchCmd = &cobra.Command{
 		defer file.Close()
 		// 读取JSON数据
 		data, err := ioutil.ReadAll(file)
-		var old map[string]Assoc
+		var jsonData map[string]Assoc
 		if err != nil {
 			fmt.Println("读取文件失败：", err)
 			//return
 		} else {
 			// 将JSON数据转换为map
-			err = json.Unmarshal(data, &old)
+			err = json.Unmarshal(data, &jsonData)
 			if err != nil {
 				fmt.Println("转换JSON失败：", err)
 				//return
 			}
 		}
-
 		first := args[0]
 		flag := cmd.Flag("all")
 		if flag.Value.String() == "true" {
-			fmt.Println("all")
-			//遍历查找，直到结束
-			next := treeTheMap(old, first)
-			log.Println("next=", next)
+			//查找过的存放在一个map里面，免得死循环
+			chainMap = make(map[string]string)
+			chainList = make([]string, 0)
+
+			next := treeTheMap(jsonData, first)
 			for next != "" {
-				next = treeTheMap(old, next)
-				log.Println("next=", next)
+				next = treeTheMap(jsonData, next)
 			}
 		} else {
-			assoc, ok := old[first]
+			assoc, ok := jsonData[first]
 			if !ok {
 				fmt.Println("没有找到关联词")
 			} else {
 				fmt.Printf("%v 和%v 关联：%v\n", assoc.Item1, assoc.Item2, assoc.Desc)
-
 			}
 		}
 
 	},
 }
 
+var chainMap map[string]string
+var chainList []string
+var index = 0
+
 func treeTheMap(m map[string]Assoc, first string) (next string) {
 	if first == "" {
 		return ""
 	}
-	log.Println(first)
 	assoc, ok := m[first]
 	if !ok {
 		fmt.Println("没有找到关联词")
 		return ""
 	} else {
-		fmt.Printf("%v 和%v 关联：%v\n", assoc.Item1, assoc.Item2, assoc.Desc)
-		//这里从Item1开始
-		fmt.Println(" assoc.Item2 != first?", assoc.Item2 != first)
-		fmt.Println(" assoc.Item1 != first?", assoc.Item1 != first)
+		_, ok2 := chainMap[first]
+		if !ok2 {
+			index = index + 1
+			chainMap[first] = assoc.Desc
+			chainList = append(chainList, first)
+		} else {
+			//打印真正的结果
+			fmt.Println("关联的词链和图像：", chainMap)
+			fmt.Println("关联的词链：", chainList)
+			return ""
+		}
 
 		if assoc.Item2 != first {
 			return assoc.Item2
@@ -105,6 +112,5 @@ func init() {
 	rootCmd.AddCommand(searchCmd)
 
 	//从一个词语关联到的下一个词语，那可能还有下下个，那么就可以一次性输出出来
-	//todo 还有死循环
-	//searchCmd.Flags().BoolP("all", "a", false, "输出一长篇记忆的词链条")
+	searchCmd.Flags().BoolP("all", "a", false, "输出一长篇记忆的词链条")
 }
